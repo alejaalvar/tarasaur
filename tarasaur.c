f59e6134aaaca74341e61f844cfa685af9878fc3
Alejandro Alvarado <alejalva@pdx.edu>
2026-01-27 18:05:03 -0800

    Add placeholder code for the help and verbose options

--- tarasaur.c
+++ tarasaur.c
@@ -20,5 +20,45 @@
 int
 main(int argc, char *argv[])
 {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "xctTVfhv")) != -1)
+    {
+        switch (opt)
+        {
+            case 'x':
+                break;
+
+            case 'c':
+                break;
+
+            case 't':
+                break;
+
+            case 'T':
+                break;
+
+            case 'V':
+                break;
+
+            case 'f':
+                break;
+
+            case 'h':
+                printf("Some help text\n");
+                break;
+
+            case 'v':
+                printf("Some verbose text\n");
+                break;
+
+            default:
+                fprintf(stderr, "Invalid command line option %c", opt);
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+
+
     return EXIT_SUCCESS;
 }
